5906200bcac5a9c55308e4d4188457e3eb3fd388
Raymond <107992151+Raymond-hub16@users.noreply.github.com>
2025-02-20 08:49:26 +0700

    fix

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title','Person Operation')
+@section('content')
+
+<h1>TEST</h1>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PersonController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::prefix('person')->name('.person')->group(function () {
+    Route::get('/', [PersonController::class,'index'])->name('index');
+    });
